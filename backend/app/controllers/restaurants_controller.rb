96b8abcbdab605abd54cc2288495992934cc063d
Jeremy Wood <js.wood206@gmail.com>
2021-04-01 21:26:40 -0700

    Updated routes and restaurant controller to find by city

--- backend/app/controllers/restaurants_controller.rb
+++ backend/app/controllers/restaurants_controller.rb
@@ -1,6 +1,10 @@
 class RestaurantsController < ApplicationController
     def index
-        restaurants = Restaurant.all
+        if !!params[:city_id]
+            restaurants = Restaurant.where(city_id: params[:city_id])
+        else
+            restaurants = Restaurant.all
+        end
         render json: RestaurantSerializer.new(restaurants)
     end
 
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :restaurants
-  resources :cities
+  resources :cities do
+    resources :restaurants, only: [:index]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
