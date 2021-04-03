b7d4beaa3ada2d2b326528df938c3633b291ad59
Jeremy Wood <js.wood206@gmail.com>
2021-04-02 23:50:35 -0700

    Added new restaurant form, set up post fetch to API

--- frontend/src/adapters/restaurantsAdapter.js
+++ frontend/src/adapters/restaurantsAdapter.js
@@ -2,6 +2,7 @@ class RestaurantsAdapter {
     constructor() {
         this.baseUrl = "http://localhost:3000"
         this.list = document.getElementById("restaurant-list")
+        this.restaurantForm = document.getElementById("restaurant-form")
         this.buttons = document.getElementById("buttons")
     }
 
@@ -30,7 +31,43 @@ class RestaurantsAdapter {
             const newButton = document.createElement(`button`)
             newButton.innerText = button
             newDiv.append(newButton)
+            newDiv.addEventListener(`click`, event => this.handleRestaurantEvent(event))
             this.buttons.append(newDiv)
         })
+    }
+
+    handleRestaurantEvent(event) {
+        if (event.target.innerText == "Add New Restaurant") {
+            this.restaurantForm.hidden = false
+        } 
+        console.log(event.target.innerText)
+    }
+
+
+    saveRestaurant(event, cityList) {
+        event.preventDefault()
+        console.log(event)
+
+        
+        const name = event.target.name.value
+        const style = event.target.style.value
+        const neighborhood = event.target.neighborhood.value
+        const notes = event.target.notes.value
+        const top_dishes = event.target.top_dishes.value
+        const city_id = cityList.value
+
+        const newRest = {name, style, neighborhood, notes, top_dishes, city_id}
+
+        let configObj = {
+            method: `POST`,
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json"
+            },
+            body: JSON.stringify(newRest)
+        }
+        fetch(this.baseUrl + '/restaurants', configObj)
+        .then(resp => resp.json())
+        .then(response => console.log(response))
     }
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,10 @@
 const citiesAdapter = new CitiesAdapter
 const restaurantsAdapter = new RestaurantsAdapter
 const cityList = document.getElementById("city-select")
+const restaurantForm = document.getElementById("restaurant-form")
+
 
 window.addEventListener("DOMContentLoaded", citiesAdapter.listCities())
 
 cityList.addEventListener("change", event => restaurantsAdapter.listRestaurants(event))
+restaurantForm.addEventListener(`submit`, event => restaurantsAdapter.saveRestaurant(event, cityList))
