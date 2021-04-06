a2081907025d31c6f12956c075f48a345d3d58d5
Jeremy Wood <js.wood206@gmail.com>
2021-04-05 18:21:16 -0700

    Added create city functionality

--- backend/app/controllers/cities_controller.rb
+++ backend/app/controllers/cities_controller.rb
@@ -3,4 +3,23 @@ class CitiesController < ApplicationController
         cities = City.all
         render json: CitySerializer.new(cities)
     end
+
+    def create
+        if City.find_by(name: params[:name])
+            render json: {error: "City already exists"}
+        else
+            city = City.new(city_params)
+            if city.save
+                render json: CitySerializer.new(city)
+            else
+                render json: {error: "ERROR"}
+            end
+        end
+    end
+
+    private
+
+    def city_params
+        params.require(:city).permit(:name)
+    end
 end
