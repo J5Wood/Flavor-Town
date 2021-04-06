54590c0b73049454f96de399d997656e586e9726
Jeremy Wood <js.wood206@gmail.com>
2021-04-05 18:21:42 -0700

    Added create city functionality

--- frontend/src/adapters/citiesAdapter.js
+++ frontend/src/adapters/citiesAdapter.js
@@ -2,6 +2,8 @@ class CitiesAdapter {
     constructor() {
         this.baseUrl = "http://localhost:3000/cities";
         this.list = document.getElementById("city-select")
+        this.newCityDiv = document.getElementById("new-city")
+        this.newCityButton = document.getElementById("create-city")
     }
     
     getCities() {
@@ -9,6 +11,9 @@ class CitiesAdapter {
     }
 
     listCities() {
+        this.list.innerHTML = `
+        <option value="none"></option>
+        `
         this.getCities().then(response => {
             response.data.forEach( city => {
                 const newOption = document.createElement("option")
@@ -17,5 +22,45 @@ class CitiesAdapter {
                 this.list.appendChild(newOption)       
             })
         })
+    }
+
+    appendCityForm() {
+        const cityForm = document.createElement("form")
+        cityForm.id = "city-form"
+        cityForm.innerHTML = `
+        <label for="city-input">New City: </label>
+        <input type="text" id="city-input">
+        <input type="submit" value="Add">
+        `
+        cityForm.addEventListener("submit", event => this.createCity(event))
+        this.newCityButton.hidden = true
+        this.newCityDiv.append(cityForm)
+    }
+
+    createCity(event) {
+        event.preventDefault()
+        const name = document.getElementById("city-input").value
+        const newCity = {name}
+
+        let configObj = {
+            method: `POST`,
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json"
+            },
+            body: JSON.stringify(newCity)
+        }
+
+        fetch(this.baseUrl, configObj)
+        .then(resp => resp.json())
+        .then(() => {
+            this.newCityButton.hidden = false
+            const cityForm = document.getElementById("city-form")
+            cityForm.remove()
+            this.listCities()
+        
+        })
+
+
     }
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,9 +2,10 @@ const citiesAdapter = new CitiesAdapter
 const restaurantsAdapter = new RestaurantsAdapter
 const cityList = document.getElementById("city-select")
 const restaurantForm = document.getElementById("restaurant-form")
-
+const newCityButton = document.getElementById("create-city")
 
 window.addEventListener("DOMContentLoaded", citiesAdapter.listCities())
 
 cityList.addEventListener("change", event => restaurantsAdapter.listRestaurants(event))
 restaurantForm.addEventListener(`submit`, event => restaurantsAdapter.saveRestaurant(event, cityList))
+newCityButton.addEventListener("click", () => citiesAdapter.appendCityForm())
