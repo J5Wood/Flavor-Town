67c4bc792ff0a449a8ac0911f7c683ba8f41e2f6
Jeremy Wood <js.wood206@gmail.com>
2021-04-07 21:39:06 -0700

    Added validations to models

--- backend/app/models/city.rb
+++ backend/app/models/city.rb
@@ -1,3 +1,5 @@
 class City < ApplicationRecord
     has_many :restaurants
+    validates :name, presence: true
+    validates :name, uniqueness: true
 end
--- backend/app/models/restaurant.rb
+++ backend/app/models/restaurant.rb
@@ -1,4 +1,5 @@
 class Restaurant < ApplicationRecord
     belongs_to :city
     serialize :top_dishes
+    validates :name, :style, :neighborhood, :notes, presence: true
 end
